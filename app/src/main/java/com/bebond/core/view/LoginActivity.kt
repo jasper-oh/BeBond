0c479529d1e6a842546100345236517f48f47201
Jasper Oh <ohyj0906@gmail.com>
2024-09-04 17:49:49 -0700

    [Feat] Intro Activity Implement Initialization

--- app/src/main/java/com/bebond/core/view/LoginActivity.kt
+++ app/src/main/java/com/bebond/core/view/LoginActivity.kt
@@ -1,7 +1,8 @@
-package com.bebond.core
+package com.bebond.core.view
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import com.bebond.core.R
 
 class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
